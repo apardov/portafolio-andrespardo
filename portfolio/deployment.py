691ee06a948850ce905ba9a6f91b8c76a0aedcb0
Andres Pardo <and.pardo.vergara@gmail.com>
2024-06-25 20:44:52 -0400

    modify deployment manage

--- portfolio/deployment.py
+++ portfolio/deployment.py
@@ -3,8 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = ['andrespardo.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://andrespardo.azurewebsites.net']
 DEBUG = False
 
 MIDDLEWARE = [
@@ -19,10 +19,10 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='): pair.split('=')[1] for pair in connection_string.split(' ')}
+connection_string = 'AZURE_POSTGRESQL_CONNECTIONSTRING'
+parameters = {pair.split('=')[0]:pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
     'default': {
